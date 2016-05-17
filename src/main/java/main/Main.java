bf4150244ee0d1c54b1606d5ba225ab3c855a023
RedChiken <gombabo1217@gmail.com>
2016-05-18 01:08:10 +0900

    AnchorPane을 이용하여 resizable한 새 UI 형성

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
         Parent root = loader.load();
 
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 550));
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
 
