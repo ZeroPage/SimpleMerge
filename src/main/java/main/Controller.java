b1093bd1593970a3b96331c02992039a92f7e052
Lyuha <beingfunnylife@gmail.com>
2016-05-20 15:37:36 +0900

    사용되지 않는 import 구문 제거, MainTest example 추가

--- src/main/java/main/Controller.java
+++ src/main/java/main/Controller.java
@@ -1,26 +1,11 @@
 package main;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.FileChooser;
-import javafx.util.Callback;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
--- src/test/java/main/MainTest.java
+++ src/test/java/main/MainTest.java
@@ -1,35 +1,21 @@
 package main;
 
-import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.api.FxToolkit.registerPrimaryStage;
-import static org.testfx.api.FxToolkit.setupApplication;
-import static org.testfx.api.FxToolkit.setupStage;
-
-import javafx.stage.Stage;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.testfx.api.FxRobot;
-import org.testfx.matcher.base.NodeMatchers;
+import org.testfx.api.FxToolkit;
 
-public class MainTest{
+public class MainTest {
     FxRobot fx = new FxRobot();
 
     @BeforeClass
-    public static void setupSpec() throws Exception {
-        Stage primaryStage = registerPrimaryStage();
-        setupStage(stage -> stage.show());
-    }
-
-    @Before
-    public void setup() throws Exception {
-        setupApplication(Main.class);
+    public static void setup() throws Exception {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.setupApplication(Main.class);
     }
 
     @Test
     public void changeTextTest() {
-        fx.clickOn("#changeButton");
-
-        verifyThat("#textArea", NodeMatchers.hasText("Change Success."));
+        fx.clickOn("#compare");
     }
 }
