0f709c8e6e16151bcf4cec9d86e177538d58516c
neropsys <neropsys@gmail.com>
2016-05-21 20:34:21 +0900

    loads utf-8 txt files

--- src/main/java/main/Controller.java
+++ src/main/java/main/Controller.java
@@ -2,15 +2,62 @@ package main;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 
+import java.awt.datatransfer.FlavorEvent;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
     private ListView<AnchorPane> textView;
+    @FXML
+    private TextArea leftView, rightView;
+    @FXML
+    private Button leftLoad, rightLoad;
+
+    @FXML
+    void RightLoadBtnAction(){
+        File file = getFile();
+        try {
+            rightView.setText(readFile(file.getPath(), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    @FXML
+    void LeftLoadBtnAction(){
+        File file = getFile();
+        try {
+            leftView.setText(readFile(file.getPath(), StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private File getFile(){
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("txt files (*.txt)", "*.txt"));
+        return chooser.showOpenDialog(null);
+
+    }
+    private String readFile(String path, Charset encoding)throws IOException{
+        byte[] content = Files.readAllBytes(Paths.get(path));
+        return new String(content, encoding);
+    }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
