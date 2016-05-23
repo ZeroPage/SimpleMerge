42ff563d12b36db225ea5ee742dbbecb1d5ea408
RedChiken <gombabo1217@gmail.com>
2016-05-23 17:29:10 +0900

    make Edit, Save Controller method

--- src/main/java/SimpleMerge/TextEditorController.java
+++ src/main/java/SimpleMerge/TextEditorController.java
@@ -6,7 +6,9 @@ import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import org.fxmisc.richtext.InlineCssTextArea;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -21,10 +23,11 @@ public class TextEditorController {
     private Button load, edit, save;
     @FXML
     private InlineCssTextArea textArea;
-
+    private String fileName;
     @FXML
     public void onLoadButtonClick() {
         File file = getFile();
+        fileName = file.getName();
         try {
             textArea.replaceText(readFile(file.getPath(), StandardCharsets.UTF_8));
             textArea.setStyle(0, "-fx-fill: red;");
@@ -44,6 +47,19 @@ public class TextEditorController {
 
     @FXML
     public void onSaveButtonClick() {
+        try{
+
+            FileWriter fw = new FileWriter(fileName == null ? "test.txt" : fileName);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(textArea.getText());
+            bw.close();
+            fw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        save.setDisable(true);
+        edit.setDisable(false);
         System.out.println( ": onSaveButtonClick()");
     }
 
