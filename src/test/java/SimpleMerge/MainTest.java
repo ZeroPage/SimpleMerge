8e38f9bc608869dba397a292946562f9ee41d861
RedChiken <gombabo1217@gmail.com>
2016-05-24 02:07:58 +0900

    test simple button click test but cannot test detail event test

--- src/test/java/SimpleMerge/MainTest.java
+++ src/test/java/SimpleMerge/MainTest.java
@@ -1,5 +1,8 @@
 package SimpleMerge;
 
+import javafx.scene.control.Button;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.testfx.api.FxRobot;
@@ -7,15 +10,37 @@ import org.testfx.api.FxToolkit;
 
 public class MainTest {
     FxRobot fx = new FxRobot();
+    CoreMatchers match = new CoreMatchers();
 
     @BeforeClass
     public static void setup() throws Exception {
         FxToolkit.registerPrimaryStage();
         FxToolkit.setupApplication(Main.class);
     }
+    @Test
+    public void loadButtonTest(){
+        fx.clickOn("#load");
+        
+    }
+    @Test
+    public void editButtonTest() {
+        fx.clickOn("#edit");
+    }
 
     @Test
-    public void changeTextTest() {
-        fx.clickOn("#compare");
+    public void saveButtonTest(){
+        fx.clickOn("#save");
+    }
+
+    @Test
+    public void editSaveTest(){
+        fx.clickOn("#edit").write("asdf");
+        fx.clickOn("#save");
+    }
+
+    @Test
+    public void loadEditSaveTest(){
+        fx.clickOn("#load");
+        //미완성
     }
 }
