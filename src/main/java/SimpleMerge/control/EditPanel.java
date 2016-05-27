2e035dc05c5d4628d546b9126465a97583f9dfac
JinGyeong Jeong <jjgjoojis@gmail.com>
2016-05-27 22:49:09 +0900

    Use EditPanel as fx:controller.

--- src/main/java/SimpleMerge/control/EditPanel.java
+++ src/main/java/SimpleMerge/control/EditPanel.java
@@ -1,9 +1,19 @@
 package SimpleMerge.control;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import org.fxmisc.richtext.InlineCssTextArea;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class EditPanel extends VBox {
     public interface EventListener {
@@ -17,6 +27,7 @@ public class EditPanel extends VBox {
     public EditPanel() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("edit_panel.fxml"));
         fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
         try {
             fxmlLoader.load();
         } catch (IOException e) {
@@ -28,27 +39,95 @@ public class EditPanel extends VBox {
         this.eventListener = eventListener;
     }
 
-    public void emitLoad() {
+    private void emitLoad() {
         if (eventListener == null)
             return;
         eventListener.onLoad();
     }
 
-    public void emitEdit() {
+    private void emitEdit() {
         if (eventListener == null)
             return;
         eventListener.onEdit();
     }
 
-    public void emitSave() {
+    private void emitSave() {
         if (eventListener == null)
             return;
         eventListener.onSave();
     }
 
-    public void emitTextChanged() {
+    private void emitTextChanged() {
         if (eventListener == null)
             return;
         eventListener.onTextChanged();
+    }
+
+    @FXML
+    private Button load, edit, save;
+    @FXML
+    private InlineCssTextArea textArea;
+    private String fileName;
+
+    @FXML
+    private void onLoadButtonClick() {
+        File file = getFile();
+        if(file != null){
+            fileName = file.getName();
+            try {
+                textArea.replaceText(readFile(file.getPath(), StandardCharsets.UTF_8));
+                textArea.setStyle(0, "-fx-fill: red;");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            edit.setDisable(false);
+            emitLoad();
+        }
+    }
+
+    @FXML
+    private void onEditButtonClick() {
+        textArea.setDisable(false);
+        edit.setDisable(true);
+        emitEdit();
+    }
+
+    @FXML
+    private void onSaveButtonClick() {
+        FileChooser fileChooser= new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("txt files (*.txt)", "*.txt"));
+        File file = fileChooser.showSaveDialog(null);
+        if(file != null){
+            try{
+                SaveFile(textArea.getText(), file);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            save.setDisable(true);
+            edit.setDisable(false);
+            emitSave();
+        }
+    }
+
+    private void SaveFile(String content, File file){
+        try{
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        }
+        catch(IOException io){
+            io.printStackTrace();
+        }
+    }
+    private File getFile(){
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("txt files (*.txt)", "*.txt"));
+        return chooser.showOpenDialog(null);
+
+    }
+    private String readFile(String path, Charset encoding)throws IOException {
+        byte[] content = Files.readAllBytes(Paths.get(path));
+        return new String(content, encoding);
     }
 }
