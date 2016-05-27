c42bd522aa785e2dac0300f0fa9ead0e1ba54d1e
JinGyeong Jeong <jjgjoojis@gmail.com>
2016-05-27 19:18:27 +0900

    Add EditPanel.EventListener interface.

--- src/main/java/SimpleMerge/control/EditPanel.java
+++ src/main/java/SimpleMerge/control/EditPanel.java
@@ -6,6 +6,14 @@ import javafx.scene.layout.VBox;
 import java.io.IOException;
 
 public class EditPanel extends VBox {
+    public interface EventListener {
+        void onLoad();
+        void onSave();
+        void onEdit();
+        void onTextChanged();
+    }
+    private EventListener eventListener;
+
     public EditPanel() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("edit_panel.fxml"));
         fxmlLoader.setRoot(this);
@@ -14,5 +22,33 @@ public class EditPanel extends VBox {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
+    }
+
+    public void emitLoad() {
+        if (eventListener == null)
+            return;
+        eventListener.onLoad();
+    }
+
+    public void emitEdit() {
+        if (eventListener == null)
+            return;
+        eventListener.onEdit();
+    }
+
+    public void emitSave() {
+        if (eventListener == null)
+            return;
+        eventListener.onSave();
+    }
+
+    public void emitTextChanged() {
+        if (eventListener == null)
+            return;
+        eventListener.onTextChanged();
     }
 }
