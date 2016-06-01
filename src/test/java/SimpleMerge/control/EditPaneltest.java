659ad3eb1fbea5c62c74d2b70ce8839428b70ad3
Lyuha <beingfunnylife@gmail.com>
2016-06-01 17:22:19 +0900

    Add EditPanelTest Fixture

---
+++ src/test/java/SimpleMerge/control/EditPaneltest.java
@@ -0,0 +1,67 @@
+package SimpleMerge.control;
+
+import javafx.application.Platform;
+import org.junit.*;
+import org.junit.rules.ExternalResource;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.util.concurrent.TimeoutException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class EditPanelTest {
+    File file = null;
+    FxRobot fx = new FxRobot();
+
+    @Rule
+    public ExternalResource resource = new ExternalResource() {
+        @Override
+        protected void after() {
+            super.after();
+            file = null;
+        }
+
+        @Override
+        protected void before() throws Throwable {
+            super.before();
+            try {
+                file = new File(this.getClass().getClassLoader().getResource("SimpleMerge/diff/multiline-1-A.txt").toURI());
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    @BeforeClass
+    public static void setUpClass() throws TimeoutException {
+        FxToolkit.registerPrimaryStage();
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws TimeoutException {
+        FxToolkit.hideStage();
+        Platform.setImplicitExit(true);
+    }
+
+    @Before
+    public void setUp() throws TimeoutException {
+        EditPanel editPanel = new EditPanel();
+        EditPanelEventListener editPanelEventListener = mock(EditPanelEventListener.class);
+        FileSelector fileSelector = mock(FileSelector.class);
+
+        when(fileSelector.getFile()).thenReturn(file);
+        editPanel.setEventListener(editPanelEventListener, fileSelector);
+
+        FxToolkit.setupSceneRoot(() -> editPanel);
+        FxToolkit.showStage();
+    }
+
+    @Test
+    public void SimpleTest() {
+        fx.clickOn("#load");
+    }
+}
--- src/test/java/SimpleMerge/control/TextEditorTest.java
+++
@@ -1,4 +0,0 @@
-package SimpleMerge.control;
-
-public class TextEditorTest {
-}
