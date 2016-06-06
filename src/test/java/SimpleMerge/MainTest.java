463b1eee4fc052205e57446bc26a5019249b98af
JinGyeong Jeong <jjgjoojis@gmail.com>
2016-06-06 22:50:17 +0900

    Add test: shouldBeAbleToLoadFile.

--- src/test/java/SimpleMerge/MainTest.java
+++ src/test/java/SimpleMerge/MainTest.java
@@ -1,18 +1,47 @@
 package SimpleMerge;
 
 import SimpleMerge.control.EditPanel;
+import SimpleMerge.control.FileSelector;
 import javafx.scene.control.Button;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.testfx.api.FxRobot;
 import org.testfx.api.FxToolkit;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.testfx.api.FxAssert.verifyThat;
 
 public class MainTest {
+    FxRobot fx = new FxRobot();
+    FileSelector leftFileSelector, rightFileSelector;
+
     @BeforeClass
     public static void setup() throws Exception {
         FxToolkit.registerPrimaryStage();
         FxToolkit.setupApplication(Main.class);
+    }
+
+    @Before
+    public void setupFileSelector() throws URISyntaxException {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+
+        EditPanel left = fx.lookup("#leftEditPanel").query();
+        EditPanel right = fx.lookup("#rightEditPanel").query();
+        leftFileSelector = mock(FileSelector.class);
+        rightFileSelector = mock(FileSelector.class);
+        URI leftResourceURI = classLoader.getResource("SimpleMerge/diff/multiline-1-A.txt").toURI();
+        URI rightResourceURI = classLoader.getResource("SimpleMerge/diff/multiline-1-B.txt").toURI();
+        when(leftFileSelector.getFile()).thenReturn(Paths.get(leftResourceURI).toFile());
+        when(rightFileSelector.getFile()).thenReturn(Paths.get(rightResourceURI).toFile());
+
+        left.setFileSelector(leftFileSelector);
+        right.setFileSelector(rightFileSelector);
     }
 
     // Side-by-Side edit panels.
@@ -40,5 +69,11 @@ public class MainTest {
     public void shouldHaveCopyToRightButtonAndCopyToLeftButton() {
         verifyThat("#leftMerge", (Button b) -> b.isDisabled() && b.getText().equals("Copy to Left"));
         verifyThat("#rightMerge", (Button b) -> b.isDisabled() && b.getText().equals("Copy to Right"));
+    }
+
+    @Test
+    public void shouldBeAbleToLoadFile() {
+        fx.clickOn("#leftEditPanel #load");
+        verifyThat("#leftEditPanel", (EditPanel editPanel) -> editPanel.getText().length() > 10);
     }
 }
