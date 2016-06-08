e439d78f56d6e1fa2c9ab6a97156da5da291cbe7
neropsys <neropsys@gmail.com>
2016-06-09 04:05:55 +0900

    Add equals, hashCode, size method for Block class.

--- src/main/java/SimpleMerge/diff/Block.java
+++ src/main/java/SimpleMerge/diff/Block.java
@@ -13,11 +13,33 @@ public class Block {
     public int end() {
         return this.end;
     }
+    public int size() {
+        return this.end - this.start;
+    }
     public void update(int offset) {
         this.update(offset, offset);
     }
     public void update(int startOffset, int endOffset) {
         this.start += startOffset;
         this.end += endOffset;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Block block = (Block) o;
+
+        if (start != block.start) return false;
+        return end == block.end;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = start;
+        result = 31 * result + end;
+        return result;
     }
 }
